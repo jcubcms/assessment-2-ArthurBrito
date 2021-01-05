15ce281be38f68cc6a66d75984885d001ccac969
= <arthurtwmt.com.au>
2021-01-06 02:09:00 +1000

    Convert Header into footer

--- bachelors-theme/bachelors-theme/footer.php
+++ bachelors-theme/bachelors-theme/footer.php
@@ -1,20 +1,23 @@
 <footer>
     <div class="container">
-        
+    <div class="copyright">
+        © <?php echo date('Y'); ?> <?php bloginfo( 'sitename' ); ?>. <?php _e('All rights reserved', '3UA'); ?>. <?php _e('This website is proundly to use WordPress', 'sitename'); ?>
+    </div>
+    <div class="container">
         <?php 
             wp_nav_menu(
                 array(
-                    "menu" => "primary",
+                    "menu" => "footer",
                     "container" => "",
-                    "theme_location" => "primary",
+                    "theme_location" => "footer",
                     "items_wrap" => '<ul id="" class="footer-menu">%3$s</ul>'
                 )
             );
         ?>
     </div>
+</footer>
     <?php
         wp_footer();
     ?>
-</footer>
 </body>
 </html>
