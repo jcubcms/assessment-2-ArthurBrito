915f850e9e43a84804bb311e8874bbff933334c2
justina823 <laisumjustina.chan@my.jcu.edu.au>
2021-01-05 20:43:52 +1000

    header with style

--- bachelors-theme/bachelors-theme/header.php
+++ bachelors-theme/bachelors-theme/header.php
@@ -12,12 +12,31 @@
 
     <body>
         <header>
+        <style>
+
+            div.custom-menu-class ul {
+            margin:20px 0px 20px 0px;
+            list-style-type: none;
+            list-style: none;
+            list-style-image: none;
+            text-align:right;
+            }
+            div.custom-menu-class li {
+                padding: 0px 20px 0px 0px;
+                display: inline;
+            } 
+            div.custom-menu-class a { 
+                color:#55555;
+            }
+        </style>
 		<?php 
    $custom_logo_id = get_theme_mod( 'custom_logo' );
    $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
       ?>
 <img src="<?php echo $image[0]; ?>" alt="">
        <div>
+       <span class='custom-logo-text'>The universtiy of the third age</span>
+       
       
 		<?php
       wp_nav_menu( array( 
