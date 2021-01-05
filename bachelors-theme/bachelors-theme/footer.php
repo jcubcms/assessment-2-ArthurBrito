ca7f17d1ac0a7809d877e7c71628eb5baa194ef6
= <arthurtwmt.com.au>
2021-01-06 00:35:58 +1000

    Basic Footer

--- bachelors-theme/bachelors-theme/footer.php
+++ bachelors-theme/bachelors-theme/footer.php
@@ -1,6 +1,20 @@
 <footer>
-I'm the footer
-<?php
-    wp_footer();
-?>
+    <div class="container">
+        
+        <?php 
+            wp_nav_menu(
+                array(
+                    "menu" => "primary",
+                    "container" => "",
+                    "theme_location" => "primary",
+                    "items_wrap" => '<ul id="" class="footer-menu">%3$s</ul>'
+                )
+            );
+        ?>
+    </div>
+    <?php
+        wp_footer();
+    ?>
 </footer>
+</body>
+</html>
